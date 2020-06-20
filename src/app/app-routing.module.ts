8bc6e6fc53df1e648b1203a2ce15bb522d8445b7
ltiuserjf61@iiht.tech <ltiuserjf61@iiht.tech>
2020-06-20 23:43:04 +0530

    project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }, { path: 'mentor', loadChildren: () => import('./mentor/mentor.module').then(m => m.MentorModule) }, { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) }, { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
